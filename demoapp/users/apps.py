43f50a484549d399a87f9178d72bb4dc3775000b
Sairam Krish <haisairam@gmail.com>
2018-06-05 16:28:24 +0530

    Initial commit

---
+++ demoapp/users/apps.py
@@ -0,0 +1,16 @@
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    name = "demoapp.users"
+    verbose_name = "Users"
+
+    def ready(self):
+        """Override this to put in:
+            Users system checks
+            Users signal registration
+        """
+        try:
+            import users.signals  # noqa F401
+        except ImportError:
+            pass
---
+++ demoapp/users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+app_name = "users"
+urlpatterns = [
+    path("", view=views.UserListView.as_view(), name="list"),
+    path("~redirect/", view=views.UserRedirectView.as_view(), name="redirect"),
+    path("~update/", view=views.UserUpdateView.as_view(), name="update"),
+    path(
+        "<str:username>/",
+        view=views.UserDetailView.as_view(),
+        name="detail",
+    ),
+]
