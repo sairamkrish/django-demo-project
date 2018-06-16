4c32ab3c61e32591f91e76ffa99d46343a4ff784
Sairam Krish <haisairam@gmail.com>
2018-06-16 16:53:47 +0530

    login user if request provides required params. This helps to logout existing user and login with required user

--- superset-conf/config/security.py
+++ superset-conf/config/security.py
@@ -11,13 +11,12 @@ class CustomAuthDBView(AuthDBView):
 
     @expose('/login/', methods=['GET', 'POST'])
     def login(self):
-        if g.user is not None and g.user.is_authenticated():
-            return redirect(self.appbuilder.get_url_for_index)
-        
         if request.args.get('username') is not None:
             user = self.appbuilder.sm.find_user(username=request.args.get('username'))
             flash('Admin auto logged in', 'success')
             login_user(user, remember=False)
+            return redirect(self.appbuilder.get_url_for_index)
+        elif g.user is not None and g.user.is_authenticated():
             return redirect(self.appbuilder.get_url_for_index)
         else:
             flash('Unable to auto login', 'warning')
